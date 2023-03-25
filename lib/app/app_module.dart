e25e83e8160fed92998b6d3a37af4bd640ee07b2
Paulo Filho <pcf.feat@gmail.com>
2023-03-25 18:09:26 -0400

    fix: move dependency injection to the correct module (home)

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,15 +1,10 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:flutter_clean_arch/app/modules/home/external/datasources/get_users_datasource_impl.dart';
-import 'modules/home/data/repositories/get_users_repository_impl.dart';
 
 import 'modules/home/home_module.dart';
 
 class AppModule extends Module {
   @override
-  final List<Bind> binds = [
-    $GetUsersDataSourceImpl,
-    $GetUsersRepositoryImpl,
-  ];
+  final List<Bind> binds = [];
 
   @override
   final List<ModularRoute> routes = [
--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,10 +1,15 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
+import 'data/repositories/get_users_repository_impl.dart';
+import 'external/datasources/get_users_datasource_impl.dart';
 import 'presentation/home/home_page.dart';
 
 class HomeModule extends Module {
   @override
-  final List<Bind> binds = [];
+  final List<Bind> binds = [
+    $GetUsersDataSourceImpl,
+    $GetUsersRepositoryImpl,
+  ];
 
   @override
   final List<ModularRoute> routes = [
