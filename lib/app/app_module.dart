1f797848008fb55361a1139a77ee3d51e0f42b94
Paulo Filho <pcf.feat@gmail.com>
2023-03-25 18:08:09 -0400

    chore: inject repository implementation into flutter modular

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_clean_arch/app/modules/home/external/datasources/get_users_datasource_impl.dart';
+import 'modules/home/data/repositories/get_users_repository_impl.dart';
 
 import 'modules/home/home_module.dart';
 
@@ -7,6 +8,7 @@ class AppModule extends Module {
   @override
   final List<Bind> binds = [
     $GetUsersDataSourceImpl,
+    $GetUsersRepositoryImpl,
   ];
 
   @override
--- lib/app/modules/home/data/repositories/get_users_repository_impl.dart
+++ lib/app/modules/home/data/repositories/get_users_repository_impl.dart
@@ -1,7 +1,12 @@
 import 'package:flutter_clean_arch/app/modules/home/data/datasources/get_users_datasource.dart';
 import 'package:flutter_clean_arch/app/modules/home/domain/models/dtos/user_dto.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 import '../../domain/repositories/get_users_repository.dart';
+
+final $GetUsersRepositoryImpl = Bind.singleton(
+  (i) => GetUsersRepositoryImpl(i()),
+);
 
 class GetUsersRepositoryImpl implements GetUsersRepository {
   GetUsersRepositoryImpl(this._getUsersDataSource);
