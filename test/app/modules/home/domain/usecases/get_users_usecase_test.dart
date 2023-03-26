539175c19c1131b9d81ed708d35047c7491aa698
Paulo Filho <pcf.feat@gmail.com>
2023-03-26 11:40:57 -0400

    test(refactor): improve setup for all main final classes

--- test/app/modules/home/domain/usecases/get_users_usecase_test.dart
+++ test/app/modules/home/domain/usecases/get_users_usecase_test.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_clean_arch/app/modules/home/data/repositories/get_users_repository_impl.dart';
 import 'package:flutter_clean_arch/app/modules/home/domain/models/dtos/user_dto.dart';
+import 'package:flutter_clean_arch/app/modules/home/domain/repositories/get_users_repository.dart';
+import 'package:flutter_clean_arch/app/modules/home/domain/usecases/get_users_usecase.dart';
 import 'package:flutter_clean_arch/app/modules/home/domain/usecases/get_users_usecase_impl.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
@@ -11,23 +13,29 @@ import 'get_users_usecase_test.mocks.dart';
 
 @GenerateNiceMocks([MockSpec<GetUsersDataSource>()])
 void main() {
-  final _getUsersDataSource = MockGetUsersDataSource();
-  final _getUsersRepositoryImpl = GetUsersRepositoryImpl(_getUsersDataSource);
-  final _getUsersUseCaseImpl = GetUsersUseCaseImpl(_getUsersRepositoryImpl);
+  late GetUsersDataSource getUsersDataSource;
+  late GetUsersRepository getUsersRepositoryImpl;
+  late GetUsersUseCase getUsersUseCaseImpl;
+
+  setUp(() {
+    getUsersDataSource = MockGetUsersDataSource();
+    getUsersRepositoryImpl = GetUsersRepositoryImpl(getUsersDataSource);
+    getUsersUseCaseImpl = GetUsersUseCaseImpl(getUsersRepositoryImpl);
+  });
 
   test('testar se foi sucesso na requisição', () async {
-    when(_getUsersDataSource()).thenAnswer((_) async => mock);
+    when(getUsersDataSource()).thenAnswer((_) async => mock);
 
-    var res = await _getUsersUseCaseImpl();
+    var res = await getUsersUseCaseImpl();
 
     expect(res.success, isTrue);
     expect(res.body, isA<List<UserDto>>());
   });
 
   test('testar se não foi sucesso na requisição', () async {
-    when(_getUsersDataSource()).thenThrow(Exception('Error'));
+    when(getUsersDataSource()).thenThrow(Exception('Error'));
 
-    var res = await _getUsersUseCaseImpl();
+    var res = await getUsersUseCaseImpl();
 
     expect(res.success, isFalse);
     expect(res.body, isNull);
