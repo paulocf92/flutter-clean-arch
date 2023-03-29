4f1dec1cfa59b969ec2e493cc9f8c442a10ea111
Paulo Filho <pcf.feat@gmail.com>
2023-03-29 13:58:40 -0400

    fix: bad certificate error

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
 
-void main() =>
-    runApp(ModularApp(module: AppModule(), child: const AppWidget()));
+class PostHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
+void main() {
+  HttpOverrides.global = PostHttpOverrides();
+  runApp(ModularApp(module: AppModule(), child: const AppWidget()));
+}
